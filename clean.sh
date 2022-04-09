0db2fdf84b2a24a4ddf062da595805fcdec8b1b7
Benoît Hubert <benoithubert@gmail.com>
2022-04-09 02:28:37 +0200

    check that clean script doesn't fail

--- clean.sh
+++ clean.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "CLEANING..."
 [[ -f build.zip ]] && rm build.zip
 [[ -d build ]] && rm -r build
 [[ -d coverage ]] && rm -r coverage
 [[ -d coverage-output ]] && rm -r coverage-output
+echo "CLEANING DONE"
